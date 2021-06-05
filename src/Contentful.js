f6a738ad6510878b6a9637578a176eea5d6d66fe
yennhi03 <yennhi97263@gmail.com>
2021-06-06 00:16:18 +0700

    - feat: contenful config

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import {createClient} from 'contentful'
+
+export default createClient({
+    space: '7cwtcupt99id',
+    accessToken: '0p8YCQo4Z8M6oEq0VTKI39ieF__VvwMjjjcaR6MoH1I'
+})
--- src/context.js
+++ src/context.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-// import items from "./data";
 import Client from "./Contentful";
-
-// Client.getEntries({ content_type: "hotelReservationSite" }).then((response) =>
-//   console.log(response.items)
-// );
 
 const RoomContext = React.createContext();
 
